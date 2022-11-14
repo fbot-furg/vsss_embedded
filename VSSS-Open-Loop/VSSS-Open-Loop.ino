a112dd09b6daa276b4b10ec0a2b1c02092705cc0
guilhermecdo <oliveiraguigo00@gmail.com>
2022-11-14 16:51:05 -0300

    bug correction

--- VSSS-Open-Loop/VSSS-Open-Loop.ino
+++ VSSS-Open-Loop/VSSS-Open-Loop.ino
@@ -40,12 +40,16 @@ numbers 1,2,3,129,130,131 are ID exclusive
 #define IDSWITCH2 PB13
 #define IDSWITCH3 PB14
 
+//Radio Pins
+#define RX PB11
+#define TX PB10
+
 uint8_t id=1;
 short radioData[2]={0,0};
 uint16_t motorSetPoint[2]={0,0};
 bool motorOrientation[2];
 
-HardwareSerial Serial3(PB11, PB10);
+HardwareSerial Serial3(RX, TX);
 
 void GetId(){
     short switchRead;
@@ -83,7 +87,7 @@ void GetId(){
 
 bool RadioRead(){
     if (Serial3.available() >= NUMOFBYTES){
-        
+
         if(Serial3.read() == id){
 
             for(short i=0; i < NUMOFBYTES; i++){
@@ -92,12 +96,14 @@ bool RadioRead(){
                 
                 if (radioData[i] >= MINCLOCKWISE && radioData[i] <= MAXCLOCKWISE){
                     motorOrientation[i]=CLOCKWISE;
-                    motorSetPoint[i]=(MAXPWM*radioData[i])/(MAXCLOCKWISE);
+                    motorSetPoint[i]=map(radioData[i],MINCLOCKWISE,MAXCLOCKWISE,MINPWM,MAXPWM);
+                    //motorSetPoint[i]=(MAXPWM*radioData[i])/(MAXCLOCKWISE);
                 }
 
                 else if (radioData[i] >= MINANTICLOCKWISE && radioData[i] <= MAXANTICLOCKWISE){
                     motorOrientation[i]=ANTICLOCKWISE;
-                    motorSetPoint[i]=(MAXPWM*radioData[i])/(MAXANTICLOCKWISE);
+                    motorSetPoint[i]=map(radioData[i],MINANTICLOCKWISE,MAXANTICLOCKWISE,MINPWM,MAXPWM);
+                    //motorSetPoint[i]=(MAXPWM*radioData[i])/(MAXANTICLOCKWISE);
                 }
 
                 else if(radioData[i] == 0){
@@ -140,16 +146,17 @@ void MotorSetup(bool motorID, bool motorOrientation, uint16_t setPoint){
 }
 
 void MoveRobot(){
-    for(short i=0;i<=2;i++){
+    for(short i=0;i<2;i++){
         MotorSetup(i,motorOrientation[i],motorSetPoint[i]);
     }
 }
 
 void setup(){
+    
     //disableDebugPorts();
     
     Serial3.begin(9600);
-    //Serial.begin(9600);
+    Serial.begin(9600);
     
     pinMode(MOTOR0PWM, OUTPUT);
     pinMode(MOTOR1PWM, OUTPUT);
@@ -162,6 +169,7 @@ void setup(){
     pinMode(IDSWITCH1, INPUT);
     pinMode(IDSWITCH2, INPUT);
     pinMode(IDSWITCH3, INPUT);
+
 }
 
 void loop(){
